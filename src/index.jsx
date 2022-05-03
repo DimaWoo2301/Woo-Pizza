6b2c91f794970af4915be6e79779d6923f440138
DimaWoo <DimaWoo2301@gmail.com>
2022-05-03 04:06:58 +0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />,
+);
+
